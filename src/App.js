399e3dccfd769b036fcdef5f36a503f6a7f25f65
Alexeytyurin143 <alexsei89tyrin@gmail.com>
2023-04-07 22:02:05 +0400

    123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="navbar">
+        <nav>Nav</nav>
+        <div className="login">
+          <img src="https://placehold.co/60x60" alt="" />
+        </div>
       </header>
+      <div className="post">
+        <div className="post__content">
+          <strong>адрес</strong>
+        </div>
+      </div>
     </div>
   );
 }
